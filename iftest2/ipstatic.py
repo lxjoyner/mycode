c47315d6881d66cb83f7dc135e5692ed010cdc24
lxjoyner <lavasier@hotmail.com>
2021-03-30 19:09:06 +0000

    working on if logic

---
+++ iftest2/ipstatic.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+## Alta3.com || Author: RZFeeser
+
+# Create variable to hold ip address
+ipchk = "192.168.0.1"
+
+# A string tests as True
+if ipchk:
+    print("Looks like the IP address was set: " + ipchk)
+
